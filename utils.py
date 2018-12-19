f4d24488e57fee476b774fb17a3e80928e04723d
amoeba <delphi1618@gmail.com>
2018-12-19 19:12:32 +0800

    init

--- utils.py
+++ utils.py
@@ -18,7 +18,19 @@ def send_text_message(id, text):
         print("Unable to send message: " + response.text)
     return response
 
+def send_img_message(id, text):
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient": {"id": id},
+        "message": {"attachment":{"type":"image","payload":{"url":text,"is_reusable":true}} }
+    }
+    response = requests.post(url, json=payload)
 
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+	
+	
 """
 def send_image_url(id, img_url):
     pass
